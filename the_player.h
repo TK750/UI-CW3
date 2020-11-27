4b49936a50a0f1b0b43d230b2acecc234f52f106
Tom Killeen <sc18twk@leeds.ac.uk>
2020-11-27 23:00:40 +0000

    Got rid of annoying reminder I put at bottom of every file

--- the_player.h
+++ the_player.h
@@ -2,6 +2,8 @@
 // Created by twak on 11/11/2019.
 //
 
+//Got rid of the timer because it was trash
+
 #ifndef CW2_THE_PLAYER_H
 #define CW2_THE_PLAYER_H
 
@@ -49,19 +51,3 @@ public slots:
 };
 
 #endif //CW2_THE_PLAYER_H
-
-
-/* widgets are objects, instances of classes
- * can be automatic allocation foo f;
- * or dynamic allocation foo* f = new foo();
- * normal to use dynamic allocation and manipulate using pointers
- *
- * containers hold pointers to widgets inside them, when parent is deleted,
- * walks through and deletes each child, when parent is enabled/disabled so are children
- *
- * parent - child relationships can be established during widget creation
- * QLabel* label = new QLabel(parentWidget);
- * or when a widget is added to a container via addWidget method
- * QLabel* label = new QLabel ();
- * label -> setParent(parentWidget);
- * -> is an operator that calls a method on a pointer to an object*/
--- tomeo.cpp
+++ tomeo.cpp
@@ -10,6 +10,8 @@
  *  2811 cw3 : twak
  */
 
+//Yeh, it's hella messy =S
+
 #include <iostream>
 #include <QApplication>
 #include <QtMultimediaWidgets/QVideoWidget>
@@ -150,20 +152,3 @@ int main(int argc, char *argv[]) {
     // wait for the app to terminate
     return app.exec();
 }
-
-
-
-/* widgets are objects, instances of classes
- * can be automatic allocation foo f;
- * or dynamic allocation foo* f = new foo();
- * normal to use dynamic allocation and manipulate using pointers
- *
- * containers hold pointers to widgets inside them, when parent is deleted,
- * walks through and deletes each child, when parent is enabled/disabled so are children
- *
- * parent - child relationships can be established during widget creation
- * QLabel* label = new QLabel(parentWidget);
- * or when a widget is added to a container via addWidget method
- * QLabel* label = new QLabel ();
- * label -> setParent(parentWidget);
- * -> is an operator that calls a method on a pointer to an object*/
