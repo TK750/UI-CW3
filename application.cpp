4d1f58896766fcefe6423f7ee3353ba8f7de03af
elizaMargaritescu <74916275+elizaMargaritescu@users.noreply.github.com>
2020-12-07 21:33:46 +0000

    grid layout - press on video, makes it fullscreen

--- application.cpp
+++ application.cpp
@@ -9,15 +9,20 @@
 //play/pause button and video's timeline has been added
 //the description of the videos has been changed to an individual description for each video
 //forward/backward 10 seconds button has been added
+//the main layout has been changed to a grid layout
+//the locations combo box has been fixed and it is now working, even though the videos are added at random - see switchLocation at the bottom of this file
 //IMPROVEMENTS: -change the text of the buttons to icons
+//              -add some styling
 //              -create more functions to adhere to the 50 lines rule
+//ISSUES: -videos are allocated at random when a combo box element is clicked
+//        -when you click once on a video it opens the new layout and it is responsive, but if you go back and click on another video, it messes up the layout
+
 
 Application::Application(vector<TheButtonInfo> videos) : videos(videos){
     //windows' properties
     setWindowTitle("tomeo");
     setMinimumSize(800, 680);
-    setMaximumWidth(2000);
-    setMaximumHeight(2000);
+    setMinimumWidth(1000);
 
     //functions used to create the application tomeo
     createWidgets();
@@ -28,94 +33,99 @@ Application::Application(vector<TheButtonInfo> videos) : videos(videos){
 void Application::createWidgets(){
 
     player->setVideoOutput(videoWidget);
+    videoWidget->hide();
 
     //list of possible locations of where the videos where taken
     //this helps organinzing the videos making them more accessbile
-    locList->addItem("Location1");
-    locList->addItem("Location2");
+    locations<<"Location1"<<"Location2";
 
     //more descriotions will be added to the list after getting the locations' combo boc working
-    descriptionList<<"Description"<<"Description2";
+    descriptions<<"Description"<<"Description2";
 
-    // a row of buttons
-    QWidget *buttonWidget = new QWidget();
+    locationsList->addItems(locations);
+    locList->addItems(descriptions);
+    connect(locationsList, SIGNAL(activated(int)), this, SLOT(switchLocation(int)));
+
     // a list of the buttons
     vector<TheButton*> buttons;
-    // the buttons are arranged horizontally
-    QVBoxLayout *layout = new QVBoxLayout();
+    // the buttons are arranged in a grid layout
     buttonWidget->setLayout(layout);
 
+    int n=0;
+    int j=0;
     //create x buttons (for x no of videos)
     for ( unsigned i = 0; i < videos.size(); i++ ) {
+        //this conditions will create the rows and columns of the grid layout
+        if(i==2){
+            n++;
+        }
+        if(i==4){
+            n++;
+        }
+        if(i==1 || i==3 || i==5){
+            j = 0;
+        }
+        else{
+            j = 1;
+        }
+
+        //the following lines are supposed to give each video a distinctive description
+        //it allocated descriptions correctly, however i could not add it to the layout without messing up the thumbnails' functionality
+
+//        QLabel *description = new QLabel();
+//        description->setText(descriptionList[0] + ' ' + (i + '0' + 1));
+
+        //creating the thumbnails for the videos
         TheButton *button = new TheButton(buttonWidget);
         button->connect(button, SIGNAL(jumpTo(TheButtonInfo* )), player, SLOT (jumpTo(TheButtonInfo* ))); // when clicked, tell the player to play.
+        connect(button, SIGNAL(clicked()), this, SLOT(fullScreen()));
+        button->setMinimumHeight(0);
         buttons.push_back(button);
-        layout->addWidget(button);
+        layout->addWidget(button,n,j);
         button->init(&videos.at(i));
-
-        //description/location of video
-        QVBoxLayout* title = new QVBoxLayout();
-        QLabel *description = new QLabel();
-        //picks the first location and numbers the videos from 1 to x (x being the no of videos available),
-        //in order to give each video an individual description
-        description->setText(descriptionList[0] + ' ' + (i + '0' + 1));
-        title->addWidget(button);
-        title->addWidget(description);
-        layout->addLayout(title);
-
+//        layout->addWidget(description,2*n+4, j);
     }
 
     // tell the player what buttons and videos are available
     player->setContent(&buttons, & videos);
 
-    scrollArea->setWidget(buttonWidget);
-    scrollArea->setMinimumWidth(390);
-    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    scrollArea->setBackgroundRole(QPalette::Light);
-
     label->setAlignment(Qt::AlignLeft);
+    label->setMaximumWidth(0);
 
-    fullScreenButton->setFixedSize(140,30);
+    fullScreenButton->setMaximumWidth(0);
     fullScreenButton->setBackgroundRole(QPalette::Light);
     connect(fullScreenButton, SIGNAL(clicked()), this, SLOT(fullScreen()));
 
+    //buttons will have 0 as default size such that they are only present when the video is played
     // play/pause button
-    playPauseButton->setFixedSize(100, 30);
+    playPauseButton->setMaximumWidth(0);
     playPauseButton->setText("Pause");
     connect(playPauseButton, SIGNAL(clicked()), this, SLOT(playAndPause()));
 
     //video timeline
     //shows the progress of the video and also we can choose what minute to play
     slider = new QSlider(Qt::Horizontal, this);
+    slider->setMaximumWidth(0);
     connect(player, &QMediaPlayer::durationChanged, slider, &QSlider::setMaximum);
     connect(player, &QMediaPlayer::positionChanged, slider, &QSlider::setValue);
     connect(slider, &QSlider::sliderMoved, player, &QMediaPlayer::setPosition);
 
     //forward 10 seconds button
-    forward->setFixedSize(70,30);
+    forward->setMaximumWidth(0);
     forward->setText("+10s");
     connect(forward, SIGNAL(clicked()), this, SLOT(seekForward()));
 
     //go backwards 10 seconds button
-    backward->setFixedSize(70,30);
+    backward->setMaximumWidth(0);
     backward->setText("-10s");
     connect(backward, SIGNAL(clicked()), this, SLOT(seekBackward()));
 
 }
 
+
 void Application::createLayout(){
 
     QVBoxLayout *top = new QVBoxLayout();
-
-    // top right - layout for the combo box and scroll area
-    QVBoxLayout* topRight = new QVBoxLayout();
-    topRight->addWidget(locList);
-    topRight->addWidget(scrollArea);
-
-    // first line of the layout, containg the video player
-    QHBoxLayout* videoTopLine = new QHBoxLayout();
-    videoTopLine->addWidget(videoWidget);
-    videoTopLine->addLayout(topRight);
 
     //layout for the player's buttons
     QHBoxLayout* buttonsLayout = new QHBoxLayout();
@@ -127,10 +137,12 @@ void Application::createLayout(){
 
     QVBoxLayout* descriptionLayout = new QVBoxLayout();
     //"Location/Description" of the video playing
+    descriptionLayout->addWidget(locationsList);
+    descriptionLayout->addWidget(buttonWidget);
     descriptionLayout->addWidget(label);
     descriptionLayout->addWidget(slider);
 
-    top->addLayout(videoTopLine);
+    top->addWidget(videoWidget);
     top->addLayout(descriptionLayout);
     top->addLayout(buttonsLayout);
 
@@ -141,14 +153,42 @@ void Application::createLayout(){
 //full screen mode function
 void Application::fullScreen() {
   // make videoWidget full screen
-  if (isFullScreen()) {
-    showNormal();
-    fullScreenButton->setText("Full screen");
+  if (isVideoFullScreen) {
+    //when the back button is pressed, the video widget is given the size of 0 such that it disappears off the screen
+    videoWidget->hide();
+    buttonWidget->show();
+    locationsList->show();
+    isVideoFullScreen=false;
+    //player's buttons will dissapear while we are in the video grid
+    playPauseButton->hide();
+    forward->hide();
+    backward->hide();
+    slider->hide();
+    fullScreenButton->hide();
+    label->hide();
   }
   //or if the widget is already in full screen mode, it makes it go back to normal
   else {
-    showFullScreen();
+    videoWidget->show();
+    buttonWidget->hide();
+    videoWidget->setMaximumWidth(2000);
+    videoWidget->setMaximumHeight(2000);
+    isVideoFullScreen=true;
     fullScreenButton->setText("Back");
+    locationsList->hide();
+    playPauseButton->setMaximumWidth(200);
+    playPauseButton->show();
+    forward->setMaximumWidth(70);
+    forward->show();
+    backward->setMinimumWidth(70);
+    backward->show();
+    slider->setMinimumWidth(800);
+    slider->show();
+    fullScreenButton->setMaximumWidth(120);
+    fullScreenButton->show();
+    label->setMaximumWidth(200);
+    label->show();
+
   }
 }
 
@@ -178,6 +218,24 @@ void Application::seekForward(){
 //backward 10 seconds button connection
 void Application::seekBackward(){
     player->setPosition(round((double)slider->value() / 10));
+}
+
+//this function makes the connection between the elements of the combo box and the videos
+//the videos are not properly distributed, hence the funciton does not work the proper way yet
+//at least it shows that we can group the videos and that the combo box works, when an element is clicked
+void Application::switchLocation(int index)
+{
+    label->setText(locList->itemText(index));
+    for ( unsigned i = 0; i < 2; i++ ) {
+        //creating the thumbnails for the videos
+        TheButton *button = new TheButton(buttonWidget);
+        button->connect(button, SIGNAL(jumpTo(TheButtonInfo* )), player, SLOT (jumpTo(TheButtonInfo* ))); // when clicked, tell the player to play.
+        connect(button, SIGNAL(clicked()), this, SLOT(fullScreen()));
+        button->setMinimumHeight(0);
+        buttons.push_back(button);
+        layout->addWidget(button);
+        button->init(&videos.at(0));
+    }
 }
 
 
